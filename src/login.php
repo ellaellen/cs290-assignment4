aafb36bb91849932063e7e4a3fd14d47535d1824
ellaellen <jinglefarmer@gmail.com>
2015-02-04 05:47:20 -0700

    done login.php html

---
+++ src/login.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>CS290 Assignment4 login</title>
+</head>
+<body>
+<form action = "content.php" method = "POST">
+<p>Please enter your username here</p>
+<input type="text" name="username">
+<input type="submit" value ="Login">
+
+</body>
+</html> 
